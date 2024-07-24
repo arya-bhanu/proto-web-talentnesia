de937a030fc48297c76c026c6487bbbbf9989757
arya-bhanu <aryacreative.dev@gmail.com>
2024-07-24 22:10:18 +0700

    feat: card section

--- modules/portal/Landing/components/ProgramDimension/ProgramDimensionCard.tsx
+++ modules/portal/Landing/components/ProgramDimension/ProgramDimensionCard.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-const ProgramDimension = () => {
-  return (
-    <div>ProgramDimension</div>
-  )
+export interface IProgramDimension {
+	title: string;
+	imgUrl: string;
 }
+const ProgramDimension = (props: IProgramDimension) => {
+	return (
+		<div className='p-5 hover:bg-[#EFF8FF] transition'>
+			<Image
+				alt='image card'
+				src={props.imgUrl}
+				width={50}
+				height={50}
+				className='object-cover rounded-full'
+			/>
+			<h4 className='font-inter mt-12'>{props.title}</h4>
+		</div>
+	);
+};
 
-export default ProgramDimension
+export default ProgramDimension;
