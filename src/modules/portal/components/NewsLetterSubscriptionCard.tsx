6033631746609e35ae9785f3ef829f93190f1674
arya-bhanu <aryacreative.dev@gmail.com>
2024-07-25 14:02:27 +0700

    feat: new card news letter subscription

--- src/modules/portal/components/NewsLetterSubscriptionCard.tsx
+++ src/modules/portal/components/NewsLetterSubscriptionCard.tsx
@@ -23,17 +23,17 @@ const NewsLetterSubscriptionCard = ({
 	const placeholderColor = `placeholder-[${color.input}]`;
 	const borderColor = `border-b border-b-[${color.input}]`;
 	return (
-		<div className={clsx(color.bg, 'rounded-3xl pl-14 py-14 flex items-end')}>
-			<div className={clsx(color.text)}>
+		<div className={clsx(color.bg, 'rounded-3xl pl-16 py-16 pr-8 flex items-end gap-12')}>
+			<div className={clsx(color.text, 'flex-[2]')}>
 				<h3 className='text-xl font-semibold font-poppins'>{title}</h3>
-				<p className='text-sm font-[400] mt-10'>{subTitle}</p>
+				<p className='text-sm font-[400] mt-9'>{subTitle}</p>
 			</div>
 			<input
 				type='email'
 				placeholder='Tulis email anda'
 				className={clsx(
 					color.text,
-					'bg-transparent outline-none',
+					'bg-transparent outline-none flex-[2] py-2 text-sm',
 					placeholderColor,
 					borderColor
 				)}
@@ -41,7 +41,7 @@ const NewsLetterSubscriptionCard = ({
 			<button
 				className={clsx(
 					actionButton.bgColor,
-					'px-6 py-2.5 rounded-full flex items-center gap-3'
+					'px-3 py-2.5 rounded-full flex items-center justify-around gap-3 flex-[1]'
 				)}
 			>
 				<span className={clsx(actionButton.textColor, 'font-medium')}>
